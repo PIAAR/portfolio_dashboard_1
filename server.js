e6598a980035870c2c226c45393c5b2b36bf7b26
PIAAR <PIAAR@rmasters.group>
2023-01-15 16:27:38 -0500

    MongoDB complete
    Mongo user controllers for auth

--- server.js
+++ server.js
@@ -2,16 +2,19 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path')
-const { logger } = require('./middleware/logger')
+const { logger, logEvents } = require('./middleware/logger')
 const errorHandler = require('./middleware/errorHandler')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const corsOptions = require('./config/corsOptions')
+const connectDB = require('./config/dbConn')
+const mongoose = require('mongoose')
 const PORT = process.env.PORT || 3500
 
 // Log message to console
 console.log(process.env.NODE_ENV)
-
+// Connect to db
+connectDB()
 // Logger first
 app.use(logger)
 
@@ -28,7 +31,8 @@ app.use('/', express.static(path.join(__dirname, 'public')))
 
 //  Root app
 app.use('/', require('./routes/root'))
-
+app.use('/users', require('./routes/userRoutes'))
+app.use('/notes', require('./routes/noteRoutes'))
 // 404 section
 app.all('*', (req, res) => {
     res.status(404)
@@ -43,5 +47,16 @@ app.all('*', (req, res) => {
 
 app.use(errorHandler)
 
+// Mongoose listener
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
+
+mongoose.connection.on('error', err => {
+    console.log(err)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+})
 // Log message to console
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+// Basic listener for DEV
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
